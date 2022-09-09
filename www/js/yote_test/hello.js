704e9e53e101379f06922f1035686ea4382b369c
Eric Wolf <coyocanid@gmail.com>
2022-09-09 16:34:32 -0700

    added compileBody to spiderpup.js

---
+++ www/js/yote_test/hello.js
@@ -0,0 +1,11 @@
+// import { yote } from '/js/yote.js'
+
+// yote.fetchApp( 'test' ).then(
+//   a => {
+//     window.yote = window.yote || {};
+//     window.yote.test = a;
+//     console.log( a, 'feea' );
+//     a.echo( 'snort' )
+//       .then( r => alert( r ) );
+//   }
+// );
